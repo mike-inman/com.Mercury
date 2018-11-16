08ca0bae560e9c1fadde2ecaf00fe5afe1e0e7b1
Michael Inman <michael.d.inman@gmail.com>
2018-11-16 16:48:26 -0600

    mapbox

--- com.Mercury/Controllers/HomeController.cs
+++ com.Mercury/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace com.Mercury.Controllers
             return View();
         }
 
+        public IActionResult MapBox()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
